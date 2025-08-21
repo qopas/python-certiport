cad8f1483b7cbfcaef486b3dd256792357f160b0
Grigoras Dumitru <grigoras.dumiitru@gmail.com>
2025-08-21 16:57:26 +0300

    fix: change " escape

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -109,11 +109,11 @@ export default function AdminDashboard() {
           `"${s.userEmail || ''}"`,
           q.questionId,
           `"${q.domain}"`,
-          `"${q.question.replace(/"/g, '""')}"`,
+          `"${q.question.replace(/"/g, '&quot;')}"`,
           `"${Array.isArray(q.userAnswer) ? q.userAnswer.join(', ') : q.userAnswer || 'Not answered'}"`,
           `"${Array.isArray(q.correctAnswer) ? q.correctAnswer.join(', ') : q.correctAnswer}"`,
           q.isCorrect,
-          `"${q.explanation.replace(/"/g, '""')}"`
+          `"${q.explanation.replace(/"/g, '&quot;')}"`
         ].join(','))
       )
     ].join('\n');
